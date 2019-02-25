11adf1d7010001ae52ab3288214032acec500391
Aditya Bhargav <adityabhargav96@gmail.com>
2019-02-25 09:31:36 +0530

    Done

--- config/secret.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-  // database: 'mongodb://testing:testing123@ds229552.mlab.com:29552/testing',
-  // database: 'mongodb://adityabhargav96:adi25071997@ds243041.mlab.com:43041/xerify',
-  database:'mongodb://localhost:27017/testverify',
-  port: 3000,
-  secretKey: "aditya"
-}
